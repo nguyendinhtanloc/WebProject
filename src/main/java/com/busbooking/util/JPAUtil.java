aa6a761dddbbe01582259ef896994c3e2ff0c2eb
Đức Thịnh <thinh17qn@gmail.com>
2025-10-13 19:52:23 +0700

    Update JPAUtil.java

--- src/main/java/com/busbooking/util/JPAUtil.java
+++ src/main/java/com/busbooking/util/JPAUtil.java
@@ -10,6 +10,18 @@ public class JPAUtil {
     
     static {
         try {
+              // Lấy biến môi trường từ hệ thống (Render inject trực tiếp)
+            Map<String, String> properties = new HashMap<>();
+            properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+            properties.put("javax.persistence.jdbc.url", System.getenv("JDBC_DATABASE_URL"));
+            properties.put("javax.persistence.jdbc.user", System.getenv("JDBC_DATABASE_USERNAME"));
+            properties.put("javax.persistence.jdbc.password", System.getenv("JDBC_DATABASE_PASSWORD"));
+
+            // Hibernate config
+            properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            properties.put("hibernate.hbm2ddl.auto", "update");
+            properties.put("hibernate.show_sql", "true");
+
             factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         } catch (Throwable ex) {
             System.err.println("Failed to create EntityManagerFactory: " + ex);
