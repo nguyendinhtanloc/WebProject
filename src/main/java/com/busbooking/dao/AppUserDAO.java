cb89aada502512cd1a1cb17b2d2c5040cfbe9fcb
Đức Thịnh <thinh17qn@gmail.com>
2025-10-14 03:11:56 +0700

    Update AppUserDAO.java

--- src/main/java/com/busbooking/dao/AppUserDAO.java
+++ src/main/java/com/busbooking/dao/AppUserDAO.java
@@ -18,7 +18,6 @@ public class AppUserDAO {
 
     public void save(AppUser user) {
         EntityManager em = emf.createEntityManager();
-        // EntityManager em = JPAUtil.getEntityManager();
         try {
             em.getTransaction().begin();
             em.persist(user);
@@ -29,7 +28,7 @@ public class AppUserDAO {
     }
 
     public AppUser update(AppUser user) {
-        // EntityManager em = JPAUtil.getEntityManager();
+        EntityManager em = JPAUtil.getEntityManager();
         EntityTransaction tx = em.getTransaction();
         try {
             tx.begin();
@@ -49,7 +48,6 @@ public class AppUserDAO {
 
     public AppUser findByEmail(String email) {
         EntityManager em = emf.createEntityManager();
-        // EntityManager em = JPAUtil.getEntityManager();
         try {
             TypedQuery<AppUser> query = em.createQuery("SELECT u FROM AppUser u WHERE u.email = :email", AppUser.class);
             query.setParameter("email", email);
@@ -62,7 +60,6 @@ public class AppUserDAO {
 
     public AppUser login(String email, String password) {
         EntityManager em = emf.createEntityManager();
-        // EntityManager em = JPAUtil.getEntityManager();
         try {
             TypedQuery<AppUser> query = em.createQuery(
                     "SELECT u FROM AppUser u WHERE u.email = :email AND u.password = :password AND u.status = 'active'",
@@ -78,7 +75,6 @@ public class AppUserDAO {
 
     public void delete(AppUser user) {
         EntityManager em = emf.createEntityManager();
-        // EntityManager em = JPAUtil.getEntityManager(); 
         try {
             em.getTransaction().begin();
             AppUser managedUser = em.find(AppUser.class, user.getUserUuid());
